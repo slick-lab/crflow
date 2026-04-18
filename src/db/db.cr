23c3c035f20c16023722e7c72328e58fb7237ce1
ZenDrx <ynwghosted@icloud.com>
2026-04-18 21:19:38 +0100

    Add database connection and posts table creation
    Initialize database connection and create posts table.

---
+++ src/db/db.cr
@@ -0,0 +1,22 @@
+require 'sqlite3'
+
+DB_File = ENV.fetch("DB_PATH", "/data/posts.db")
+
+def db 
+  @@db ||= DB.open "sqlite3://#{DB_FILE}"
+end 
+
+def create_tables
+  db.exec <<-SQL
+   CREATE TABLE IF NOT EXISTS posts (
+     id INTEGER PRIMARY KEY,
+     title TEXT NOT NULL,
+     url TEXT NOT NULL,
+     context TEXT NOT NULL,
+     sumbitter_name TEXT NOT NULL,
+     submitter_email TEXT NOT NULL,
+     created_at TEXT NOT NULL
+    )
+  SQL
+end 
+  
